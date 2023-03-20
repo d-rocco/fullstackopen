9e4fc130087ba6fd6a5d4b146b4dc872dba662f2
d-rocco <dhrocco27@gmail.com>
2023-03-20 15:39:36 -0400

    Complete exercise 1.8-1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,12 +6,21 @@ const Button = ({ clickHandler, name }) => {
   return <button onClick={clickHandler}>{name}</button>;
 };
 
-const Stat = ({ text, num }) => {
+const Stat = (props) => {
+  if (props.stats === 0) {
+    return;
+  }
   return (
     <div>
-      {text} {num}
+      {props.text} {props.num}
     </div>
   );
+};
+
+const History = ({ stats }) => {
+  if (stats === 0) {
+    return <h3>No feedback given</h3>;
+  }
 };
 
 const App = () => {
@@ -19,22 +28,43 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allStats, setStats] = useState(0);
   const feedbackHeader = "give feedback";
   const statisticsHeader = "statistics";
+
+  const handleGood = () => {
+    setGood(good + 1);
+    setStats(allStats + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setStats(allStats + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+    setStats(allStats + 1);
+  };
 
   return (
     <div>
       <Header title={feedbackHeader} />
-      <Button clickHandler={() => setGood(good + 1)} name="good" />
-      <Button clickHandler={() => setNeutral(neutral + 1)} name="neutral" />
-      <Button clickHandler={() => setBad(bad + 1)} name="bad" />
+      <Button clickHandler={handleGood} name="good" />
+      <Button clickHandler={handleNeutral} name="neutral" />
+      <Button clickHandler={handleBad} name="bad" />
       <Header title={statisticsHeader} />
-      <Stat text="good" num={good} />
-      <Stat text="neutral" num={neutral} />
-      <Stat text="bad" num={bad} />
-      <Stat text="all" num={good + neutral + bad} />
-      <Stat text="average" num={(good - bad) / (good + neutral + bad)} />
-      <Stat text="positive" num={good / (good + neutral + bad) + " %"} />
+      <History stats={allStats} />
+      <Stat stats={allStats} text="good" num={good} />
+      <Stat stats={allStats} text="neutral" num={neutral} />
+      <Stat stats={allStats} text="bad" num={bad} />
+      <Stat stats={allStats} text="all" num={allStats} />
+      <Stat stats={allStats} text="average" num={(good - bad) / allStats} />
+      <Stat
+        stats={allStats}
+        text="positive"
+        num={(good / allStats) * 100 + " %"}
+      />
     </div>
   );
 };
