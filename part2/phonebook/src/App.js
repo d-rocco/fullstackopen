93a77270450a27c71e7284c33db00e95bf120c06
d-rocco <dhrocco27@gmail.com>
2023-03-21 17:35:57 -0400

    Complete exercise 2.7-2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,24 +2,39 @@ import { useState } from "react";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
-  const addName = (event) => {
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const addPerson = (event) => {
     event.preventDefault();
-    const nameObj = { name: newName };
-    setPersons(persons.concat(nameObj));
+    const names = persons.map((person) => `${person.name.toLowerCase()}`);
+    if (newName !== "" && !names.includes(newName.toLowerCase())) {
+      const personObj = { name: newName, number: newNumber };
+      setPersons(persons.concat(personObj));
+    } else {
+      alert(`${newName} is already added to phonebook!`);
+    }
     setNewName("");
+    setNewNumber("");
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,15 +1,19 @@
 const Persons = ({ persons }) => {
   return (
-    <div>
+    <>
       {persons.map((person) => (
-        <Person key={person.name} name={person.name} />
+        <Person key={person.name} name={person.name} number={person.number} />
       ))}
-    </div>
+    </>
   );
 };
 
 const Person = (props) => {
-  return <div>{props.name}</div>;
+  return (
+    <div>
+      {props.name} {props.number}
+    </div>
+  );
 };
 
 export default Persons;
